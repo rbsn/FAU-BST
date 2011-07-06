a54402e6976c0fc95f133e72cfd3a1a985304183
Conami <conami@conami-AMILO-Xi-2428.(none)>
2011-07-06 21:18:27 +0200

    Removed lines of code that are commend out
    	modified:   queue.cc
    	modified:   spinlock.cc
    	modified:   tip.cc

--- trunk/queue.cc
+++ trunk/queue.cc
@@ -1,8 +1,3 @@
-// $Date: 2009-09-22 15:20:27 +0200 (Tue, 22 Sep 2009) $, $Revision: 2221 $, $Author: benjamin $
-// kate: encoding ISO-8859-15;
-// vim: set fileencoding=latin-9:
-// -*- mode: c; coding: latin-9 -*-
-
 #include "queue.h"
 
 
--- trunk/spinlock.cc
+++ trunk/spinlock.cc
@@ -1,25 +1,12 @@
 #include "spinlock.h"
 
-//static volatile int owner;
-
 Spinlock::Spinlock() {
 	__sync_lock_release( &isLocked );
 }
 
 void Spinlock::lock() {
-	//int i = 0;
 	
-	while( __sync_lock_test_and_set( &isLocked, 1 ) ) { 
-		//i++; 
-		//if(i == 1000) {
-		//	volatile int tip = organizer.active()->cid;
-		//	break;
-		//}
-	}
-
-	
-	// owner = organizer.active()->cid;	
-
+	while( __sync_lock_test_and_set( &isLocked, 1 ) ) { }
 }
 
 void Spinlock::unlock() {
